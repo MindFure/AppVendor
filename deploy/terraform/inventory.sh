3dcb876d0e9faa9de7117777ce6e5a1b88b773a7
Дмитрий Юзов <dima-zop2012@yandex.ru>
2026-06-16 23:57:27 +0700

    обновил инвентори сш

--- deploy/terraform/inventory.sh
+++ deploy/terraform/inventory.sh
@@ -7,11 +7,16 @@ if [ -z "$VM_INTERNAL_IP" ] || [ -z "$EXTERNAL_IP" ]; then
   exit 1
 fi
 
-# Генерируем inventory.yaml (или hosts.ini) с группой [webservers]
+# Генерируем inventory файл с динамическим путем
+mkdir -p ../ansible/inventory
 cat << EOF > ../ansible/inventory/hosts.ini
 [webservers]
 appvendor-prod ansible_host=${EXTERNAL_IP} ansible_user=dyuzov ansible_ssh_common_args='-o StrictHostKeyChecking=no'
 
 [webservers:vars]
 internal_ip=${VM_INTERNAL_IP}
+project_root="/home/dyuzov/var/www/AppVendor"
+ansible_python_interpreter=/usr/bin/python3
 EOF
+
+echo "Inventory сгенерирован для IP: ${EXTERNAL_IP}"
